b792f6f11e900b672c58e2cd5e0efb7d7ae736a7
ErwanQui <erwan.quinot@gmail.com>
2023-03-16 11:39:11 +0100

    try to create clickable image add all menus to nav_view (but fragment not added yet)

--- app/src/main/java/edu/devmo/frigonnecte/ui/calendrier/CalendrierFragment.java
+++ app/src/main/java/edu/devmo/frigonnecte/ui/calendrier/CalendrierFragment.java
@@ -1,6 +1,7 @@
 package edu.devmo.frigonnecte.ui.calendrier;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import java.util.logging.Logger;
 
 import edu.devmo.frigonnecte.databinding.FragmentCalendrierBinding;
 
@@ -22,16 +25,17 @@ public class CalendrierFragment extends Fragment {
                 new ViewModelProvider(this).get(CalendrierViewModel.class);
 
         binding = FragmentCalendrierBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textCalendrier;
-        calendrierViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void clickImage(View view) {
+        Log.i("ok", "c'est ok");
     }
 }
