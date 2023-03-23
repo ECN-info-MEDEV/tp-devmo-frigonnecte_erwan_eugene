fc94f0cda82ca3b112f3533fc991600a59775f81
ErwanQui <erwan.quinot@gmail.com>
2023-03-23 18:07:35 +0100

    update import

--- app/src/main/java/edu/devmo/frigonnecte/ui/recettes/RecettesViewModel.java
+++ app/src/main/java/edu/devmo/frigonnecte/ui/recettes/RecettesViewModel.java
@@ -3,6 +3,7 @@ package edu.devmo.frigonnecte.ui.recettes;
 import android.app.Application;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.io.Closeable;
